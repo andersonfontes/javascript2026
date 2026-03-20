73f02c48b66b331b3cc67b55b0a7fc9bcaef9f1e
anderson_fontes <and@and>
2026-03-20 16:49:42 -0300

    solucao desafio while

--- while.js
+++ while.js
@@ -1,17 +1,33 @@
 // WHILE IRA RODAR AQUELE CODIGO **ENQUANTO** A CONDIÇÃO FOR VERDADEIRA
 const prompt = require("prompt-sync")()
 
-let numerosecreto = "8"
+let numerosecreto = 8
+let tentativas = 1 //CONTADOR DE TENTATIVAS
 
-let palpite = prompt("__Adivinhe o numero secreto: ")
+console.log("TENTATIVA: " + tentativas)
+
+let palpite = Number(prompt("__Adivinhe o numero secreto: "))
 if (palpite == numerosecreto) {
     console.log("__ACERTOU !!! PARABENS")
 }
 
 while (palpite != numerosecreto) {
-    console.log("\n VOCE ERROU")
+    console.log("\n VOCE ERROU!! ")
+
+    if(palpite > numerosecreto){  
+        console.log("tente mais alto")      
+    }
+    else{
+        console.log("tente mais baixo")
+    }
+
+    tentativas++
+    if (tentativas == 5) {
+        console.log("ACABARAM AS TENTATIVAS !!")
+    }
 
     console.log("*** NOVA TENTATIVA ***")
+    console.log("TENTATIVA: " + tentativas)
     palpite = prompt("\nAdivinhe o numero secreto: ")
     
     if (palpite == numerosecreto) {
